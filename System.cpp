344be1195e7dd753b32089b93651f2741e7c6836
Reynaldo-Falcon <falcon.reynaldo20@gmail.com>
2025-05-29 20:28:34 -0400

    Opened up starter files, began creating constructors for System.cpp

---
+++ System.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+
+//--------------------------
+// Description:
+//--------------------------
+
+class System
+{
+private:
+    // --- Forces ---
+    vector<float> forceComp;    // Force Components
+    vector<forceComp> forceList;    // Force list
+
+    // --- Torques ---
+    vector<float> torqueComp;   // Torque Components
+    vector<torqueComp> torqueList;  // Torque list
+
+public:
+    System(vector<forceComp> forceList, vector<torqueComp> torqueList)
+}
+
+
+// Make constructor for a system that includes the forces and torques that act on it, and its initial position and velocity.
+// This will allow me to update velocity and position accordingly
+void main()
+{
+
+}
+
